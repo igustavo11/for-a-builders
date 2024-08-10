21e342c44619db0ce56a8a28d5a096339fd78dd4
igustavo11 <131629449+igustavo11@users.noreply.github.com>
2024-08-09 22:15:26 -0300

    commit teste 2

---
+++ js/dados.js
@@ -0,0 +1,29 @@
+var PORTIFOLIO = {
+    
+    "all":[
+        {
+            "id": "casa teste",
+            "img": "./img/casa2.jpg",
+            "dsc": "casa1",
+        },
+        {
+            
+            "id": "casa teste",
+            "img": "./img/casa2.jpg",
+            "dsc": "casa1",
+
+        },
+        {
+            "id": "casa teste",
+            "img": "./img/casa2.jpg",
+            "dsc": "casa1",
+        },
+        {
+            
+            "id": "casa teste",
+            "img": "./img/casa2.jpg",
+            "dsc": "casa1",
+
+        },
+    ]
+}
